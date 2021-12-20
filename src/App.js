c32aca99ac5226d9b908ae0a5b05edadc8a0abd6
Aline Silva <lisie_su@yahoo.com.br>
2021-12-20 12:21:11 +0000

    Display message if list is empty

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import ItemsLeft from "./components/ItemsLeft";
+import { useSelector } from "react-redux";
 
 function App() {
+  const todos = useSelector((state) => state.todos);
+
+  {/* Display list of todos if quatity greater than 0, otherwise show h3 message */}
+  let content = <h3>There are no todos on your list</h3>;
+
+  if (todos.length > 0) {
+    content = <TodoList />;
+  }
   return (
     <div>
       <section id="form">
@@ -11,8 +20,8 @@ function App() {
         <TodoForm />
       </section>
       <section id="todos">
-        <TodoList />
-        <ItemsLeft />
+        {content}
+        {todos.length > 0 && <ItemsLeft />}
       </section>
     </div>
   );
